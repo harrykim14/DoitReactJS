8e2fc1afd0384bf7557b544506bd0ef0e8eef602
harrykim14 <harrykim14@korea.ac.kr>
2020-10-13 02:07:23 +0900

    Studying ReactJS with storybook
    20.10.13.

--- storybookexample/src/stories/InputStory.jsx
+++ storybookexample/src/stories/InputStory.jsx
@@ -3,5 +3,7 @@ import { storiesOf } from '@storybook/react';
 
 import Input from '../03/Input';
 
-storiesOf('Input', module).add('기본 설정', () => <Input />);
+storiesOf('Input', module)
+    .add('기본 설정', () => <Input name = "name" />)
+    .add('label 예제', () => <Input name = "name" label = "이름" />);
 
